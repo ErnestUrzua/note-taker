c686078d99be7fa2e8e57a81af8afc7e15059dfd
ErnestUrzua <ernieurzua@gmail.com>
2020-07-19 15:26:05 -0700

    created server files and routes

--- public/assets/js/apiRoutes.js
+++ public/assets/js/apiRoutes.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var NotesDb = require("../data/db");
+var NotesDb = require("db/db.json");
 
 
 // ===============================================================================
