21d2a4702e2f9328b56dc7db0776f41f4415ea6d
LoneStar <ernieurzua@gmail.com>
2020-07-20 08:11:21 -0700

    fixed api get functions

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -3,6 +3,8 @@
 var express = require("express");
 var path = require("path");
 var NotesDb = require("../../db/db.json");
+var fs = require("fs");
+
 
 // EXPRESS CONFIGURATION
 // Tells node that we are creating an "express" server
@@ -21,37 +23,38 @@ app.use(express.static('public'));
 // Declaring a counter to keep track of the current ID for new notes
 var idCounter = 0;
 
+
 // HTML GET Requests-------------------------------------------------------------------
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "../../../public/notes.html"));
+    res.sendFile(path.join(__dirname, "../../../public/notes.html"));
 });
 
 // If no matching route is found default to index
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../../../public/index.html"));
+    res.sendFile(path.join(__dirname, "../../../public/index.html"));
 });
 
 // API GET Requests-------------------------------------------------------------------
+//gets json.db and returns as text data
 app.get("/api/notes", function (req, res) {
-  return res.json(NotesDb);
+    fs.readFile(NotesDb, "utf8", (err, data) => {
+        if (err) throw err;
+        var notes = JSON.parse(data);
+    })
+    return res.json(notes);
 });
 
 //API Delete 
 app.delete("/api/notes/:id", function (req, res) {
-  res.send(req.params)
-  res.json(NotesDb);
+    res.send(req.params)
+    res.json(NotesDb);
 });
 
 
 // API POST Requests
-
 app.post("/api/notes", function (req, res) {
-  // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-  // It will do this by sending out the value "true" have a table
-  // req.body is available since we're using the body parsing middleware
-
-  NotesDb.push(req.body);
-  res.json(true);
+    NotesDb.push(req.body);
+    res.json(NotesDb);
 
 });
 
@@ -59,7 +62,8 @@ app.post("/api/notes", function (req, res) {
 // LISTENER
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
-  });
+});
+
 
 
 
