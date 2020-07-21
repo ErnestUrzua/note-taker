5bb057ed05e05f0d57e2ffb3ed1e974684ebb43e
ErnestUrzua <ernieurzua@gmail.com>
2020-07-20 22:55:45 -0700

    api.get retrieves Json object and pushes to db

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -18,7 +18,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //app.use(express.static('../../../public'));
-app.use(express.static(path.join(__dirname,'../../../public')))
+app.use(express.static(path.join(__dirname, '../../../public')))
 
 
 // Declaring a counter to keep track of the current ID for new notes
@@ -35,18 +35,20 @@ app.get("/notes", function (req, res) {
 // API GET Requests-------------------------------------------------------------------
 //gets json.db and returns as text data
 app.get("/api/notes", function (req, res) {
-    console.log('Hello from the api notes route')
+    console.log('called api/notes')
     fs.readFile(NotesDb, "utf8", (err, data) => {
         if (err) throw err;
         var notes = JSON.parse(data);
+        console.log("retriving notes from db")
+        console.log(notes)
         return res.json(notes)
     })
-    
+
 });
 
 //API Delete 
 app.delete("/api/notes/:id", function (req, res) {
-   
+
 
     //ONE RESPONSE
 });
@@ -56,25 +58,25 @@ app.delete("/api/notes/:id", function (req, res) {
 //Should recieve a new note to save on the request body, 
 //add it to the `db.json` file, and then return the new note to the client.
 app.post("/api/notes", function (req, res) {
-
-    // var newNote = {}
-    // newNote.id = idCounter
-    // newNote.text = req.body.text
-    // newNote.title = req.body.title
-
-    // var notesDb = fs.readFilea-->)
-    // NotesDb.push(newNote);
-
-    // idCounter++
-
-    // savefile
-
+    console.log("app.post reached");
     fs.readFile(NotesDb, "utf8", (err, data) => {
         if (err) throw err;
         var notes = JSON.parse(data);
+        console.log("retriving notes from db")
+        console.log(notes)
     })
-    return res.json(notes);
 
+    idCounter++;//increment the count of posts
+
+    var newNote = {id:idCounter, title: req.body.title,text: req.body.text}
+    console.log("app.post newNote");
+    console.log(newNote);
+
+    //stringfy object to send over to server
+    var notes = JSON.stringify(newNote);
+    NotesDb.push(notes);//push into db
+    // savefile and return the json object
+    return res.json(NotesDb);
 });
 
 // If no matching route is found default to index
